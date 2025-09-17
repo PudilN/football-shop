170a117b901d6a39b5464b05022ca4bb4bd98a3a
PudilN <ainurfadhil32@gmail.com>
2025-09-17 11:09:31 +0700

    PLS FINAL

--- main/views.py
+++ main/views.py
@@ -14,7 +14,7 @@ def show_main(request):
         'app' : 'Chill Kicks',
         'name': 'Ainur Fadhil',
         'class': 'PBP F',
-        'products': productsdawd
+        'products': products
     }
 
     return render(request, "main.html", context)
